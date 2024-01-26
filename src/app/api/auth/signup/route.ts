618bd53be64b6fc8528ff9f99292743b7d47b137
Shaif Arfan <shaifarfan08@gmail.com>
2024-01-26 17:56:32 +0600

    udpate error handle for prisma

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,6 +2,7 @@ import { handleError } from '@/lib/handleError';
 import { NextRequest, NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import prisma from '@/lib/prisma';
+import { z } from 'zod';
 
 export async function GET() {
   return Response.json({ message: 'passed' }, { status: 200 });
@@ -11,18 +12,34 @@ export async function POST(req: NextRequest) {
   try {
     const salt = bcrypt.genSaltSync(10);
     const body = await req.json();
-    const password = bcrypt.hashSync(body.password, salt);
+
+    const res = z
+      .object({
+        email: z.string().email(),
+        password: z.string().min(3),
+        name: z.string().min(2),
+      })
+      .safeParse(body);
+
+    if (!res.success) {
+      return NextResponse.json(res.error, { status: 400 });
+    }
+    console.log(res.data);
+    const password = bcrypt.hashSync(res.data.password, salt);
+    console.log({ password });
     const user = await prisma.user.create({
       data: {
-        email: body.email,
+        email: res.data.email,
         password,
-        name: body.name,
+        name: res.data.name,
       },
       select: {
         email: true,
         name: true,
       },
     });
+
+    console.log(user);
     return NextResponse.json(user, { status: 201 });
   } catch (e) {
     return handleError(e);
--- src/lib/handleError.ts
+++ src/lib/handleError.ts
@@ -3,10 +3,28 @@ import { NextResponse } from 'next/server';
 
 export const handleError = (e: any) => {
   if (e instanceof Prisma.PrismaClientKnownRequestError) {
+    switch (e.code) {
+      case 'P2002':
+        return NextResponse.json(
+          {
+            message: `Already ${e.meta?.modelName} exist`,
+            target: e.meta?.target,
+            error: e,
+          },
+          { status: 400 }
+        );
+      default:
+        return NextResponse.json(e.message, { status: 500 });
+    }
+  }
+  if (e instanceof Prisma.PrismaClientKnownRequestError) {
     return NextResponse.json(e.meta?.cause, { status: 500 });
+  }
+  if (e instanceof Prisma.PrismaClientValidationError) {
+    return NextResponse.json(e.message, { status: 500 });
   }
   if (e instanceof Error) {
     return NextResponse.json(e.message, { status: 500 });
   }
-  return NextResponse.json(e, { status: 500 });
+  return NextResponse.json('Something Went Wrong', { status: 500 });
 };
