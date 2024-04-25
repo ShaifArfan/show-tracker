8542e875f41f51c1f5611e4de80b9e147026b88b
Shaif Arfan <shaifarfan08@gmail.com>
2024-04-25 22:51:41 +0600

    updated show episode update form

--- src/app/actions/episode.ts
+++ src/app/actions/episode.ts
@@ -1,7 +1,9 @@
 'use server';
 
+import prisma from '@/lib/prisma';
 import { updateEpisodeWatch } from '@/server/query/episode';
 import { revalidatePath } from 'next/cache';
+import { z } from 'zod';
 
 export async function updateEpisodeWatchAction({
   watched,
@@ -21,4 +23,90 @@ export async function updateEpisodeWatchAction({
     console.error(e);
     throw new Error('Failed to update episode!!');
   }
+}
+
+// TODO move it to queries
+export const getLastEpisodeNum = async ({
+  showId,
+  seasonNum,
+}: {
+  showId: number;
+  seasonNum: number;
+}) => {
+  const lastEpi = await prisma.episode.findFirst({
+    where: {
+      showId,
+      seasonNumber: seasonNum,
+    },
+    orderBy: {
+      episodeNumber: 'desc',
+    },
+  });
+  return lastEpi?.episodeNumber || 0;
+};
+
+// TODO also add a higher limit
+const ADD_REMOVE_EPISODES = z.object({
+  epiAmount: z.number().gt(0).lte(100),
+  seasonNum: z.number().gt(0),
+  showId: z.number(),
+});
+
+interface Add_Remove_Episodes extends z.infer<typeof ADD_REMOVE_EPISODES> {}
+
+export async function addEpisodes(data: Add_Remove_Episodes) {
+  const parseResult = await ADD_REMOVE_EPISODES.safeParse(data);
+  if (!parseResult.success) {
+    // TODO need to think about better error handling
+    throw parseResult.error;
+  }
+
+  const { epiAmount, seasonNum, showId } = parseResult.data;
+
+  const lastEpisodeNum = await getLastEpisodeNum({
+    seasonNum,
+    showId,
+  });
+
+  const newEpisodes = new Array(epiAmount).fill(null).map((_, i) => ({
+    seasonNumber: seasonNum,
+    episodeNumber: i + lastEpisodeNum + 1,
+    showId,
+  }));
+
+  const createdEps = await prisma.episode.createMany({
+    data: newEpisodes,
+  });
+  revalidatePath(`/shows/${showId}`);
+  return createdEps;
+}
+
+export async function removeEpisodes(data: Add_Remove_Episodes) {
+  const parseResult = await ADD_REMOVE_EPISODES.safeParse(data);
+  if (!parseResult.success) {
+    // TODO need to think about better error handling
+    throw parseResult.error;
+  }
+  const { epiAmount, seasonNum, showId } = parseResult.data;
+  const lastEpisodeNum = await getLastEpisodeNum({
+    seasonNum,
+    showId,
+  });
+
+  if (lastEpisodeNum < epiAmount) {
+    throw new Error('Not enough episodes to delete');
+  }
+
+  const delEps = await prisma.episode.deleteMany({
+    where: {
+      showId,
+      seasonNumber: seasonNum,
+      episodeNumber: {
+        gt: lastEpisodeNum - epiAmount,
+      },
+    },
+  });
+
+  revalidatePath(`/shows/${showId}`);
+  return delEps;
 }
--- src/components/ShowForm.tsx
+++ src/components/ShowForm.tsx
@@ -1,8 +1,9 @@
+import { addEpisodes, removeEpisodes } from '@/app/actions/episode';
 import { Button, Group, NumberInput, Select } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { notifications } from '@mantine/notifications';
 import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import useSWRMutation from 'swr/mutation';
+import { useState } from 'react';
 
 interface FormValues {
   epiAmount: number;
@@ -11,14 +12,6 @@ interface FormValues {
 }
 
 type FormAction = 'add' | 'remove';
-
-const updateEpis = async (
-  url: string,
-  { arg }: { arg: { epiAmount: number; seasonNum: number; action: FormAction } }
-) => {
-  const res = await axios.put(url, arg);
-  return res;
-};
 
 interface Props {
   activeTab: string | null;
@@ -33,6 +26,7 @@ interface Props {
 }
 
 function ShowForm({ activeTab, setActiveTab, seasons, showId }: Props) {
+  const [mutating, setMutating] = useState(false);
   const form = useForm<FormValues>({
     initialValues: {
       epiAmount: 1,
@@ -41,33 +35,72 @@ function ShowForm({ activeTab, setActiveTab, seasons, showId }: Props) {
     },
   });
 
-  const { trigger, isMutating } = useSWRMutation(
-    `/api/show/${showId}`,
-    updateEpis
-  );
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const res = await trigger({
-      epiAmount: form.values.epiAmount,
-      seasonNum: form.values.seasonNum,
-      action: form.values.action,
-    });
-    if (res?.status !== 202) return;
-    if (form.values.action === 'add') {
-      if (form.values.seasonNum > seasons.length) {
-        setActiveTab(`s${form.values.seasonNum}`);
+    setMutating(true);
+    try {
+      switch (form.values.action) {
+        case 'add': {
+          await addEpisodes({
+            epiAmount: form.values.epiAmount,
+            seasonNum: form.values.seasonNum,
+            showId,
+          });
+          notifications.show({
+            title: 'Success',
+            message: 'Episodes Added',
+            color: 'green',
+          });
+          break;
+        }
+        case 'remove': {
+          await removeEpisodes({
+            epiAmount: form.values.epiAmount,
+            seasonNum: form.values.seasonNum,
+            showId,
+          });
+          notifications.show({
+            title: 'Success',
+            message: 'Episodes removed',
+            color: 'green',
+          });
+          break;
+        }
+        default:
+          break;
       }
-    } else if (form.values.action === 'remove') {
-      const isAllDel =
-        seasons[
-          seasons.findIndex((val) => val.seasonNumber === form.values.seasonNum)
-        ]._count._all === form.values.epiAmount;
-      if (isAllDel && activeTab === `s${form.values.seasonNum}`) {
-        setActiveTab(`s${seasons[0].seasonNumber}` || null);
+
+      // set tabs
+      if (form.values.action === 'add') {
+        if (form.values.seasonNum > seasons.length) {
+          setActiveTab(`s${form.values.seasonNum}`);
+        }
+      } else if (form.values.action === 'remove') {
+        const isAllDel =
+          seasons[
+            seasons.findIndex(
+              (val) => val.seasonNumber === form.values.seasonNum
+            )
+          ]._count._all === form.values.epiAmount;
+        if (isAllDel && activeTab === `s${form.values.seasonNum}`) {
+          setActiveTab(`s${seasons[0].seasonNumber}` || null);
+        }
       }
+    } catch (err) {
+      const myError =
+        err instanceof Error ? err : new Error('Failed to update episode!!');
+      notifications.show({
+        title: 'Failed to update episode!!',
+        message: myError.message,
+        color: 'red',
+      });
+
+      console.error('fallback error', err);
+    } finally {
+      setMutating(false);
     }
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <Group>
@@ -78,7 +111,7 @@ function ShowForm({ activeTab, setActiveTab, seasons, showId }: Props) {
           data={['add', 'remove']}
           {...form.getInputProps('action')}
         />
-        <Button type="submit" disabled={isMutating}>
+        <Button type="submit" disabled={mutating} loading={mutating}>
           Submit
         </Button>
       </Group>
