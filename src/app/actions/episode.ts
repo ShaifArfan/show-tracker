fa1adf226966f3488ad8417de8010b1106bd4086
Shaif Arfan <shaifarfan08@gmail.com>
2024-04-29 19:41:22 +0600

    fillerEp server action

--- src/app/actions/episode.ts
+++ src/app/actions/episode.ts
@@ -45,7 +45,6 @@ export const getLastEpisodeNum = async ({
   return lastEpi?.episodeNumber || 0;
 };
 
-// TODO also add a higher limit
 const UPDATE_EPISODES_PROPS = z.object({
   epiAmount: z.number().gt(0).lte(10000),
   seasonNum: z.number().gt(0),
@@ -142,4 +141,87 @@ export const rangeWatchEpisodes = async (data: Update_Episodes_Props) => {
 
   revalidatePath(`/shows/${showId}`);
   return episodes;
+};
+
+const UPDATE_FILLER_PROPS = z.object({
+  showId: z.number().int(),
+  season: z.number().int().gt(0),
+  fillerList: z.string(),
+});
+interface Update_Filler_Props extends z.infer<typeof UPDATE_FILLER_PROPS> {}
+
+export const updateFiller = async (data: Update_Filler_Props) => {
+  const parseResult = await UPDATE_FILLER_PROPS.safeParse(data);
+  if (!parseResult.success) {
+    // TODO need to think about better error handling
+    throw parseResult.error;
+  }
+
+  const { showId, season, fillerList } = parseResult.data;
+  const fillerEpArray = [] as number[];
+  const failedEp = [] as any[];
+  const fillerEpSlipt = fillerList.split(',').map((item) => item);
+
+  fillerEpSlipt.forEach((item) => {
+    if (Number(item) && Number.isInteger(Number(item)) && Number(item) > 0) {
+      // positive integer
+      const epi = Number(item);
+      if (!fillerEpArray.includes(epi)) {
+        fillerEpArray.push(epi);
+      }
+    } else if (
+      !Number(item) &&
+      item.includes('-') &&
+      item.split('-').length === 2
+    ) {
+      // it's a range
+      const range = item.split('-');
+      const start = parseInt(range[0], 10);
+      const end = parseInt(range[1], 10);
+      if (start < 0 || end < 0) {
+        failedEp.push(item);
+        return;
+      }
+      for (let i = start; i <= end; i += 1) {
+        fillerEpArray.push(i);
+      }
+    } else {
+      failedEp.push(item);
+    }
+  });
+
+  const eps = await prisma.episode.updateMany({
+    where: {
+      showId,
+      seasonNumber: season,
+      episodeNumber: {
+        in: fillerEpArray,
+      },
+    },
+    data: {
+      isFiller: true,
+    },
+  });
+
+  await prisma.episode.updateMany({
+    where: {
+      showId,
+      seasonNumber: season,
+      episodeNumber: {
+        notIn: fillerEpArray,
+      },
+    },
+    data: {
+      isFiller: false,
+    },
+  });
+
+  revalidatePath(`/shows/${showId}`);
+  return {
+    show_id: showId,
+    season,
+    update_count: eps.count,
+    fillerList: fillerEpArray.join(', '),
+    failedEp: failedEp.join(', '),
+  };
 };
