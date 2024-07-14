edbc1bdc3b877cc20f326fd9cc5dfdba88685cb2
Shaif Arfan <shaifarfan08@gmail.com>
2024-07-14 14:49:35 +0600

    updated analytics mobile scrollbar default position to right

--- src/components/analytics/Heatmap.tsx
+++ src/components/analytics/Heatmap.tsx
@@ -1,18 +1,25 @@
 'use client';
 
 import { fetcher } from '@/lib/swrFetcher';
-import { Box, Tooltip } from '@mantine/core';
-import React from 'react';
+import { Box, ScrollArea, Tooltip } from '@mantine/core';
+import React, { useEffect } from 'react';
 import useSWR from 'swr';
 
 const levelColors = ['#4e4e4e76', '#39436d', '#5a71d0', '#3860ff', '#4967ff'];
 
 function Heatmap() {
+  const ref = React.useRef<HTMLDivElement>(null);
   const {
     data,
     isLoading,
     error: fetchError,
   } = useSWR<Record<string, number>>(`/api/episode/analytics`, fetcher);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollLeft = ref.current.scrollWidth;
+    }
+  }, []);
 
   if (isLoading) {
     return 'loading...';
@@ -28,39 +35,41 @@ function Heatmap() {
   const stats = data;
 
   return (
-    <Box
-      style={{
-        maxWidth: '928px',
-        width: '100%',
-        overflow: 'scroll',
-        display: 'flex',
-        flexDirection: 'column',
-        height: '90px',
-        flexWrap: 'wrap',
-        gap: '3px',
-        rowGap: '3px',
-      }}
-    >
-      {Object.keys(stats).map((item) => {
-        // const level = Math.round((Math.random() * 10) / ((max - min) / 4));
-        const level = Math.round(stats[item] / ((max - min) / 4));
-        return (
-          <Tooltip label={`${item}: ${stats[item]}`}>
-            <Box
-              bg={levelColors[level] || levelColors[0]}
-              display="inline-block"
-              w={12}
-              h={12}
-              ta="center"
-              p={2}
-              style={{
-                borderRadius: '2px',
-              }}
-            />
-          </Tooltip>
-        );
-      })}
-    </Box>
+    <ScrollArea type="always" scrollbarSize={6} viewportRef={ref}>
+      <Box
+        style={{
+          maxWidth: '928px',
+          width: '100%',
+          // overflow: 'scroll',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '90px',
+          flexWrap: 'wrap',
+          gap: '3px',
+          rowGap: '3px',
+        }}
+      >
+        {Object.keys(stats).map((item) => {
+          // const level = Math.round((Math.random() * 10) / ((max - min) / 4));
+          const level = Math.round(stats[item] / ((max - min) / 4));
+          return (
+            <Tooltip label={`${item}: ${stats[item]}`}>
+              <Box
+                bg={levelColors[level] || levelColors[0]}
+                display="inline-block"
+                w={12}
+                h={12}
+                ta="center"
+                p={2}
+                style={{
+                  borderRadius: '2px',
+                }}
+              />
+            </Tooltip>
+          );
+        })}
+      </Box>
+    </ScrollArea>
   );
 }
 
